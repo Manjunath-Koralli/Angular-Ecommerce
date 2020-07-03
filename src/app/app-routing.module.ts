62743ae0f0b50874959ddd2a1cd80da6106851f0
Manjunath Koralli <mskoralli@gmail.com>
2020-07-03 19:47:48 +0530

    added product details component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,14 +2,15 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ProductListComponent } from './components/product-list/product-list.component';
 import { SearchComponent } from './components/search/search.component';
+import { ProductDetailsComponent } from './components/product-details/product-details.component';
 
 
 const routes: Routes = [
-  { path:'products', component: ProductListComponent},
+  { path:'products/:id', component: ProductDetailsComponent},
   { path:'products/search/:keyword', component: ProductListComponent},
   { path:'category/:id', component: ProductListComponent},
   { path:'category', component: ProductListComponent},
-  
+  { path:'products', component: ProductListComponent},
   { path:'', redirectTo:'/products', pathMatch:'full'},
   { path:'**', redirectTo:'/products', pathMatch:'full'},
 ];
