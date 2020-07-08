d8f66bd08f84b85a968a80b78eb6d66546f1991f
Manjunath Koralli <mskoralli@gmail.com>
2020-07-09 00:08:58 +0530

    Added remove feature

--- src/app/components/cart-details/cart-details.component.ts
+++ src/app/components/cart-details/cart-details.component.ts
@@ -47,4 +47,8 @@ export class CartDetailsComponent implements OnInit {
     this.cartService.decrementQuantity(cartItem);
   }
 
+  remove(cartItem : CartItem){
+    this.cartService.remove(cartItem);
+  }
+
 }
