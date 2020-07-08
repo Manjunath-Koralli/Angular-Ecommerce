857a5d611cb6c6d560f73b142747f623ff089e84
Manjunath Koralli <mskoralli@gmail.com>
2020-07-08 23:13:11 +0530

    added cart details

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,9 +3,11 @@ import { Routes, RouterModule } from '@angular/router';
 import { ProductListComponent } from './components/product-list/product-list.component';
 import { SearchComponent } from './components/search/search.component';
 import { ProductDetailsComponent } from './components/product-details/product-details.component';
+import { CartDetailsComponent } from './components/cart-details/cart-details.component';
 
 
 const routes: Routes = [
+  { path:'cart-details', component: CartDetailsComponent},
   { path:'products/:id', component: ProductDetailsComponent},
   { path:'products/search/:keyword', component: ProductListComponent},
   { path:'category/:id', component: ProductListComponent},
